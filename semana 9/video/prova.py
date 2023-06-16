ffa706f8231ccbc83e8f2ac5385a710a1c22d669
Antoniointeli <antonio.teixeira@sou.inteli.edu.br>
2023-06-16 16:23:37 -0300

    correcao_video_out
    Correção do vídeo de output

--- semana 9/video/prova.py
+++ semana 9/video/prova.py
@@ -1,3 +1,4 @@
+#importa as bibliotecas open cv e urllib, para uso do framework e de repostirórios online, respectivamente
 import cv2
 import urllib.request
 
@@ -5,6 +6,7 @@ import urllib.request
 url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
 
 # Caminho de destino para salvar o arquivo XML
+#Esse arquivo contém o padrão já treinado de faces do haarcascade
 cascade_path = "haarcascade_frontalface_default.xml"
 
 # Faz o download do arquivo XML
@@ -16,13 +18,8 @@ face_cascade = cv2.CascadeClassifier(cascade_path)
 # Especifique o caminho do arquivo de vídeo
 video_path = cv2.VideoCapture('videos/arsene.mp4')
 
-# Inicializa a captura de vídeo
-
-
-
-# Como foi possível abrir o video de entrada, vamos agora utilizar 
-# essa captura para definir o tamanho do video de saida
-width  = int(video_path.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
+# Define as especificações do vídeo de saída
+width  = int(video_path.get(cv2.CAP_PROP_FRAME_WIDTH))  
 height = int(video_path.get(cv2.CAP_PROP_FRAME_HEIGHT))
 output_video = cv2.VideoWriter( 'out.avi',cv2.VideoWriter_fourcc(*'DIVX'), 24, (width, height))
 
@@ -40,7 +37,7 @@ while True:
     # Passa o detector em cascata pelo método multi scale
     faces = face_cascade.detectMultiScale(
         gray_frame,
-        scaleFactor=1.05,
+        scaleFactor=1.06,
         minNeighbors=1
     )
 
@@ -50,11 +47,14 @@ while True:
 
     # Mostra o quadro resultante
     cv2.imshow('Video', frame)
+    # Escreva o vídeo frame a frame em um vídeo normal
+    output_video.write(frame)
 
-    # Verifica se a tecla 'q' foi pressionada para sair do loop
+    # Verifica se a tecla 'f' foi pressionada para sair do loop, quando pressionada, o usuário para a visualização
     if cv2.waitKey(1) & 0xFF == ord('f'):
         break
 
 # Libera os recursos
 video_path.release()
+output_video.release()
 cv2.destroyAllWindows()
