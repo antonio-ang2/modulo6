521058757c0bae95e60f3a6d12cbfa346862306e
Antoniointeli <antonio.teixeira@sou.inteli.edu.br>
2023-06-19 20:49:02 -0300

    estilização
    estilização e complemento da explicação.

--- exercicio_semana7/ponderadas/ponderada.py
+++ exercicio_semana7/ponderadas/ponderada.py
@@ -1,26 +1,24 @@
+#importação das bibliotecas
 import os
 import time
 from flask import Flask, request
 from supabase import create_client, Client
-
+#criação de um servidor em Pyhton
 app = Flask(__name__)
-
 # Chave de acesso ao supabase
 url = "https://qycjcdntwivlombvmwsf.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Y2pjZG50d2l2bG9tYnZtd3NmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NzE5NTY4NywiZXhwIjoyMDAyNzcxNjg3fQ.KmlbtEVcN9gZOsC1TgwcNhuq23q8rFaMuOcfcgPEUkg"
-
 # Cria o cliente para conectar na API do supabase
 supabase: Client = create_client(url, key)
-# Estabelece o nome do bucket
+# Seta o nome do bucket criado na URL em questão
 bucket_name = "images"
-
 # Rota para realizar upload de imagens
-@app.route("/upload", methods=["GET", "POST"])
+@app.route("/mandar", methods=["GET", "POST"])
 def upload_image():
     if request.method == "POST":
         # Pega o arquivo correspondente ao upload que o usuário realizou via interface
         image = request.files["image"]
-        # Lê o arquivo de imagem recebido
+        # Lê o arquivo de imagem recebido pelo front
         data = image.read()
         # Cria um padrão de nome para as imagens enviadas
         filename = f"{time.time()}_{image.filename}"
@@ -36,20 +34,49 @@ def upload_image():
     # Retornando o HTML fornecido
     return '''
         <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Ponderada 4</title>
-        </head>
-        <body>
-            <h1>Coloque sua imagem aqui</h1>
-            <form method="post" enctype="multipart/form-data">
-                <input type="file" name="image">
-                <input type="submit" value="Upload">
-            </form>
-        </body>
-        </html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ponderada 4</title>
+    <style>
+        /* Estilização do input */
+        input[type="file"] {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f8f8f8;
+            color: #333;
+            font-size: 16px;
+            margin-bottom: 10px;
+        }
+        
+        /* Estilização do botão de submit */
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        
+        /* Estilização do botão de submit quando o mouse passa por cima */
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <h1>Envie sua imagem aqui</h1>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
+
     '''
 
 if __name__ == '__main__':
